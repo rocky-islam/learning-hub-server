fa2d21e5ff37bd73903fbe2ba47da79835b83e78
rocky-islam <62227570+rocky-islam@users.noreply.github.com>
2022-10-25 08:39:56 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const cors = require('cors')
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+
+const data = require('./data/data.json')
+
+app.get('/', (req, res) => {
+    res.send('API Running');
+});
+
+app.get('/data', (req, res) =>{
+    res.send(data);
+})
+
+app.listen(port, () => {
+    console.log('server running on port:', port);
+    
+})
